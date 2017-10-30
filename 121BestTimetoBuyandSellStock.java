a521aeb8989d02f049e28cfb8374ccdc49f4622e
Zoro <ZoroSpace@users.noreply.github.com>
2017-10-30 17:48:02 +0800

    Update 121BestTimetoBuyandSellStock.java

--- 121BestTimetoBuyandSellStock.java
+++ 121BestTimetoBuyandSellStock.java
@@ -1,24 +1,22 @@
 class Solution {
     public int maxProfit(int[] prices) {
-        if(prices.length == 0) {
-            return 0;
-        }
-        else {
-            int[] minNum = new int[prices.length];
-            minNum[0] = prices[0];
-            int result = 0;
+        if(prices.length == 0)  return 0;
 
-            for(int i = 0;i < prices.length - 1;i++) {
-                if(minNum[i] < prices[i+1]) {
-                    minNum[i+1] = minNum[i];
+        int[] minNum = new int[prices.length];
+        minNum[0] = prices[0];
+        int result = 0;
 
-                } else {
-                    minNum[i+1] = prices[i+1];
-                }
-                result = result > prices[i+1]-minNum[i+1]?result:prices[i+1]-minNum[i+1];
+        for(int i = 0;i < prices.length - 1;i++) {
+            if(minNum[i] < prices[i+1]) {
+                minNum[i+1] = minNum[i];
+
+            } else {
+                minNum[i+1] = prices[i+1];
             }
-            return result;
+            result = result > prices[i+1]-minNum[i+1]?result:prices[i+1]-minNum[i+1];
         }
+        return result;
+
         
     }
 }
