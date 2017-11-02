b348ee820cbaa3b3266c51a9ef1d6665e7a04125
Zoro <ZoroSpace@users.noreply.github.com>
2017-11-02 21:45:33 +0800

    Update 069Sqrt(x).java

--- 069Sqrt(x).java
+++ 069Sqrt(x).java
@@ -1,6 +1,19 @@
 class Solution {
     public int mySqrt(int x) {
-        if(x < 0) return x;
-        return (int)Math.sqrt(x);
+        if(x <= 1) return x;
+        int lo = 1;
+        int hi = x;
+        int mid;
+        while(lo < hi) {
+            mid = (lo + hi)/2;
+            if(mid == x/mid) return mid;
+            if(mid < x/mid) {
+                if(mid * 2 < lo + hi) lo = mid + 1;
+                else lo = mid;
+            } else {
+                hi = mid;
+            }
+        }
+        return hi - 1;
     }
 }
