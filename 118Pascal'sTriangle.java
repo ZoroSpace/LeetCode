6f5bf23c11c8a633fa68631905bc5b705f511370
Zoro <ZoroSpace@users.noreply.github.com>
2017-11-03 23:36:47 +0800

    Create 118Pascal'sTriangle.java

---
+++ 118Pascal'sTriangle.java
@@ -0,0 +1,16 @@
+class Solution {
+    public List<List<Integer>> generate(int numRows) {
+        List<Integer> thisLevel;
+        List<List<Integer>> result = new LinkedList<LinkedList<Integer>>();
+        for(int i = 0;i<numRows;i++) {
+            thisLevel = new LinkedList<Integer>();
+            if(i % 2 == 1) {
+                for(int j = 0;j < (i+1)/2;j++) {
+                    thisLevel.add
+                }
+            } else {
+                
+            }
+        }
+    }
+}
