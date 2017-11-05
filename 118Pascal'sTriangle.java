864c9f2222edcc779c1a3b26acf38f27ad6b175f
Zoro <ZoroSpace@users.noreply.github.com>
2017-11-05 09:21:17 +0800

    Update 118Pascal'sTriangle.java

--- 118Pascal'sTriangle.java
+++ 118Pascal'sTriangle.java
@@ -5,20 +5,20 @@ class Solution {
         int[] previousLevel = {0};
         for(int i = 0;i < numRows;i++) {
             currentLevel = new int[i+1];
-            if(numRows == 1) {
+            if(i == 0) {
                 currentLevel[0] = 1;
-            } else if(numRows == 2) {
+            } else if(i == 1) {
                 currentLevel[0] = 1;
                 currentLevel[1] = 1;
             } else {
                 currentLevel[0] = 1;
-                for(int j = 1;j < i - 1;j++) {
+                for(int j = 1;j < i;j++) {
                     currentLevel[j] = previousLevel[j-1]+previousLevel[j];
                 }
-                currentLevel[i - 1] = 1;
+                currentLevel[i] = 1;
             }
             List<Integer> tempList = new LinkedList<Integer>();
-            for(int j = 0;j < i;j++) {
+            for(int j = 0;j < i+1;j++) {
                 tempList.add(currentLevel[j]);
             }
             result.add(tempList);
