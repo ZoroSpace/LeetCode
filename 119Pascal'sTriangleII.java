ff6cb4c0f16b8920c530ac45260c4695358035e5
Zoro <ZoroSpace@users.noreply.github.com>
2017-11-05 21:58:37 +0800

    Update 119Pascal'sTriangleII.java

--- 119Pascal'sTriangleII.java
+++ 119Pascal'sTriangleII.java
@@ -1,16 +1,23 @@
 class Solution {
     public List<Integer> getRow(int rowIndex) {
         List<Integer> result = new LinkedList<>();
-        for(int i = 0;i < rowIndex+1;i++) {
-            result.add(combination(rowIndex,i));
+        int[] temp = new int[rowIndex+1];
+        for(int i = 0;i < rowIndex/2+1;i++) {
+            temp[i] = combination(rowIndex,i);
+        }
+        for(int i = rowIndex/2+1;i < rowIndex+1;i++) {
+            temp[i]=temp[rowIndex-i];
+        }
+        for(int i = 0;i < temp.length;i++) {
+            result.add(temp[i]);
         }
         return result;
     }
     int combination(int n,int k) {
         if(n == k || k == 0) return 1;
-        
         if(k > n/2) return combination(n,n-k);
-        
-        return combination(n,k-1) * (n-k+1)/k;
+        double c = Math.max(combination(n,k-1)/k * (n-k+1),(n-k+1)/k * combination(n,k-1));
+        c = Math.max(c,(n-k+1) * combination(n,k-1)/k);
+        return (int)c;
     }
 }
