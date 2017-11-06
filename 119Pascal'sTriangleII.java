011fec7ad87392dd99aec939bce18658c2acef68
Zoro <ZoroSpace@users.noreply.github.com>
2017-11-06 12:49:10 +0800

    Update 119Pascal'sTriangleII.java

--- 119Pascal'sTriangleII.java
+++ 119Pascal'sTriangleII.java
@@ -17,3 +17,22 @@ class Solution {
         return result;
     }
 }
+// class Solution {
+//     public List<Integer> getRow(int rowIndex) {
+//         List<Integer> result = new LinkedList<>();
+//         long[] temp = new long[rowIndex+1];
+//         temp[0] = 1;
+//         long c;
+//         for(int i = 1;i < rowIndex/2+1;i++) {
+//             c = temp[i-1] * (rowIndex-i+1)/i;
+//             temp[i] = c;
+//         }
+//         for(int i = rowIndex/2+1;i < rowIndex+1;i++) {
+//             temp[i]=temp[rowIndex-i];
+//         }
+//         for(int i = 0;i < temp.length;i++) {
+//             result.add((int)temp[i]);
+//         }
+//         return result;
+//     }
+// }
