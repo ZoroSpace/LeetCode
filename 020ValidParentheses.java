39b456de1b821ec4e11ee455fa923aba81d57f5b
Zoro <ZoroSpace@users.noreply.github.com>
2017-11-10 11:24:00 +0800

    Create 020ValidParentheses.java

---
+++ 020ValidParentheses.java
@@ -0,0 +1,21 @@
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack();
+        for(int i = 0;i < s.length();i++) {
+            if(s.charAt(i) == '(' || s.charAt(i) == '[' || s.charAt(i) == '{') {
+                stack.push(s.charAt(i));
+            } else if(s.charAt(i) == ')') {
+                if(stack.empty()) return false;
+                if(stack.pop() != '(') return false;
+            } else if(s.charAt(i) == ']') {
+                if(stack.empty()) return false;
+                if(stack.pop() != '[') return false;
+            } else if(s.charAt(i) == '}') {
+                if(stack.empty()) return false;
+                if(stack.pop() != '{') return false;
+            }
+        }
+        if(stack.empty()) return true;
+        else return false;
+    }
+}
