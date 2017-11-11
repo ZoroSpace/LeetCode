49bb74ae454d67bba0460163b4671d49ac908db2
Zoro <ZoroSpace@users.noreply.github.com>
2017-11-11 12:00:27 +0800

    Create 014LongestCommonPrefix.java

---
+++ 014LongestCommonPrefix.java
@@ -0,0 +1,19 @@
+class Solution {
+    public String longestCommonPrefix(String[] strs) {
+        if(strs.length == 0) return "";
+        String result = strs[0];
+        for(int i = 0;i < strs.length;i++) {
+            result = helper(strs[i],result);
+        }
+        return result;
+    }
+    public String helper(String s1,String s2) {
+        StringBuilder sb = new StringBuilder();
+        int length = s1.length() < s2.length()?s1.length():s2.length();
+        for(int i = 0;i < length;i++) {
+            if(s1.charAt(i) == s2.charAt(i)) sb.append(s1.charAt(i));
+            else break;
+        }
+        return sb.toString();
+    }
+}
