518145b0ac79e65209cd1527e263af39a0609c8c
Zoro <ZoroSpace@users.noreply.github.com>
2017-11-11 23:40:16 +0800

    Create 028ImplementstrStr().java

---
+++ 028ImplementstrStr().java
@@ -0,0 +1,16 @@
+class Solution {
+	public int strStr(String haystack,String needle) {
+        if(needle.length() == 0) return 0;
+		for(int i = 0;i < haystack.length() - needle.length()+1;i++) {
+			for(int j = 0;j < needle.length();j++) {
+				if(haystack.charAt(i+j) != needle.charAt(j)) {
+					break;
+				} else {
+					if(j == needle.length() - 1) return i;
+				}
+			}
+		}
+    return -1;
+	}
+}
+
