af7a4847e41dc05a04238c336509871bf9da4ad3
Zoro <ZoroSpace@users.noreply.github.com>
2017-11-28 22:11:26 +0800

    Update 022GenerateParentheses.java

--- 022GenerateParentheses.java
+++ 022GenerateParentheses.java
@@ -1,16 +1,39 @@
 class Solution {
-	public List<String> generateParenthesis(int n) {
-		Stack<Character> lsk = new Stack<>();
-		Stack<Character> rsk = new Stack<>();
-		
-		
-	}
-	String helper(Stack<Character> lsk,Stack<Character> rsk) {
-		if(lsk.size() = rsk.size()) {
-			return lsk.pop() + helper(lsk,rsk);
-		} else if(lsk.size() == 0) {
-			return rsk.pop() + helper(lsk,rsk);
-		} else if(rsk.size() == 0) {
-			return lsk.pop() + helper(lsk,rsk);
-	}
+    public List<String> generateParenthesis(int n) {
+        LinkedList<String> strings = new LinkedList<>();//offer() poll()
+        if(n == 0) {
+            strings.add("");
+            return strings;
+        }
+        LinkedList<Integer> nums = new LinkedList<>();
+        strings.offer("(");
+        nums.offer(1);
+        int stringSize;
+        int numSize;
+        String s;
+        int counter;
+        for(int i = 0;i < 2*n - 1;i++) {
+            stringSize = strings.size();
+            for(int j = 0;j < stringSize;j++) {
+                s = strings.poll();
+                counter = nums.poll();
+                if(counter + i == 2 * n - 1) {
+                    strings.offer(s+")");
+                    nums.offer(counter - 1);
+                }else if(counter == 0) {
+                    counter++;
+                    strings.offer(s+"(");
+                    nums.offer(1);
+                } else {
+                    strings.offer(s+")");
+                    nums.offer(counter - 1);
+                    strings.offer(s+"(");
+                    nums.offer(counter + 1);
+                }
+            }
+        }
+        return strings;
+        
+    }
+    
 }
