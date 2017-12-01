97e8003925294dd37daf29ba8bc4c8fd297b27a7
Zoro <ZoroSpace@users.noreply.github.com>
2017-12-01 15:28:48 +0800

    Update 039CombinationSum.java

--- 039CombinationSum.java
+++ 039CombinationSum.java
@@ -10,24 +10,35 @@ class Solution {
         int i;
         for(i = n-1;i >= -1;i--) {
             if(i == -1) {
-                if(x == 0) return result;
-                else return null;
+                return null;
             }
             if(c[i] <= x) break;
         }
+        int[] subC = new int[i];
+        System.arraycopy(c,0,subC,0,i);
         if(c[i] == x) {
             List<Integer> list = new LinkedList<>();
             list.add(x);
             result.add(list);
-        }
-        int subC = new int[i];
-        System.arraycopy(c,0,subC,0,i);
-        for(List l1 : helper(subC,x)) {
-            result.add(l1);
-        }
-        for(List l1 : helper(subC,x-c[i])) {
-            l1.add(c[i]);
-            result.add(l1);
+            if(helper(subC,x) != null) {
+                for(List l1 : helper(subC,x)) {
+                    result.add(l1);
+                }
+            } return result;
+        } else {
+            if(helper(subC,x) != null) {
+                for(List l1 : helper(subC,x)) {
+                    result.add(l1);
+                }
+            }
+            if(helper(subC,x-c[i]) != null) {
+                for(List l1 : helper(subC,x-c[i])) {
+                    l1.add(c[i]);
+                    result.add(l1);
+                }
+                return result;
+            }
+            return null;
         }
     }
 }
