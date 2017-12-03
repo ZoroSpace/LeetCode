e39f25e779d9c091cb2d52f9fa31b6d621f398aa
Zoro <ZoroSpace@users.noreply.github.com>
2017-12-03 15:08:29 +0800

    Update 024SwapNodesinPairs.java

--- 024SwapNodesinPairs.java
+++ 024SwapNodesinPairs.java
@@ -8,18 +8,21 @@
  */
 class Solution {
     public ListNode swapPairs(ListNode head) {
+        if(head == null || head.next == null) return head;
         ListNode first = head;
         ListNode second = head.next;
         ListNode third;
-        ListNode result = head.next;
-        while(second.next != null) {
+        head = second;
+        while(second.next != null && second.next.next != null) {
             third = second.next;
             first.next = second.next.next;
             second.next = first;
             second = first.next;
             first = third;
         }
+        first.next = second.next;
         second.next = first;
-        return result;
+        return head;
+        
     }
 }
