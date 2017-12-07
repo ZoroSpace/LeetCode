b23fa9d96072701d97a1bc15cacc234fded2a31c
Zoro <ZoroSpace@users.noreply.github.com>
2017-12-07 12:03:58 +0800

    Update 011ContainerWithMostWater.java

--- 011ContainerWithMostWater.java
+++ 011ContainerWithMostWater.java
@@ -1,30 +1,20 @@
 class Solution {
     public int maxArea(int[] nums) {
-        Map<Integer,Integer> map = new TreeMap<>();
-        for(int i = 0;i < nums.length;i++) {
-            map.put(nums[i],i);
-        }
-        Integer integ;
-        int ai;
+        if(nums.length <= 1) return 0;
         int left = 0;
-        int right = nums.length-1;
+        int right = 1;
         int result = 0;
-        Iterator iter = map.entrySet().iterator();
-        int[] flag = new int[nums.length];
-        int[] c = new int[nums.length];
-        int i = 0;
-        
-        while(iter.hasNext()) {
-            Map.Entry entry = (Map.Entry)iter.next();
-            ai = (Integer)entry.getKey();
-            integ = (Integer)entry.getValue();
-            result = Math.max(result,ai*(integ-left));
-            result = Math.max(result,ai*(right-integ));
-            flag[integ] = 1;
-            while(flag[left] == 1) left++;
-            while(flag[right] == 1) right--;
-            if(left == right) break;
+        for(int i = 1;i < nums.length;i++) {
+            for(int j = 0;j < i;j++) {
+                if(nums[j] != 0 ) {
+                    result = Math.max(result,(i-j)*Math.min(nums[i],nums[j]));
+                }
+            }
+            if(nums[i] <= nums[left]) nums[i] = 0;
+            else {
+                left = i;
+            }
         }
-        return c[0];
+        return result;
     }
 }
