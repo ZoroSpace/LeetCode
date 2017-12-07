c1c1163f4d81bd7da56d2fce844da627dbf7e604
Zoro <ZoroSpace@users.noreply.github.com>
2017-12-07 12:33:31 +0800

    Update 011ContainerWithMostWater.java

--- 011ContainerWithMostWater.java
+++ 011ContainerWithMostWater.java
@@ -1,18 +1,19 @@
 class Solution {
     public int maxArea(int[] nums) {
         if(nums.length <= 1) return 0;
-        int left = 0;
-        int right = 1;
         int result = 0;
+        int[][] valid = new int[2][nums.length];
+        valid[0][0] = 0;
+        valid[1][0] = nums[0];
+        int counter = 0;
         for(int i = 1;i < nums.length;i++) {
-            for(int j = 0;j < i;j++) {
-                if(nums[j] != 0 ) {
-                    result = Math.max(result,(i-j)*Math.min(nums[i],nums[j]));
-                }
+            for(int j = 0;j < counter+1;j++) {
+                result = Math.max(result,(i-valid[0][j])*Math.min(nums[i],valid[1][j]));
             }
-            if(nums[i] <= nums[left]) nums[i] = 0;
-            else {
-                left = i;
+            if(nums[i] > valid[1][counter]) {
+                counter++;
+                valid[0][counter] = i;
+                valid[1][counter] = nums[i];
             }
         }
         return result;
