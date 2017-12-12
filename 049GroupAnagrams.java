d91934f041feea47cdf149c701e46bf0f21fb302
Zoro <ZoroSpace@users.noreply.github.com>
2017-12-12 16:14:23 +0800

    Update and rename 49GroupAnagrams.java to 049GroupAnagrams.java

--- 049GroupAnagrams.java
+++ 049GroupAnagrams.java
@@ -1,30 +1,28 @@
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
-        List<List<String>> result = new LinkedList<>();
-        if(strs.length == 0) return result;
-        else return helper(strs);
-    }
-    
-    List<List<String>> helper(String[] strs) {
         List<List<String>> lastResult = new LinkedList<>();
+        if(strs.length == 0) return new LinkedList<>();
         String cur = strs[strs.length - 1];
         String[] lastStrs = new String[strs.length - 1];
         System.arraycopy(strs,0,lastStrs,0,strs.length - 1);
-        lastResult = helper(lastStrs);
+        lastResult = groupAnagrams(lastStrs);
         String cpr;//compare
-        char[] cprChar;
+        char[] cprChar1;
+        char[] cprChar2;
         for(List<String> list : lastResult) {
-            list = (LinkedList)list;
-            cpr = list.getFirst;
-            cprChar = cahrcpr.toCharArray();
-            Arrays.sort(cprChar);
-            if(String.valueOf(cprChar) == cur) {
+            cpr = ((LinkedList<String>)list).getFirst();
+            cprChar1 = cpr.toCharArray();
+            cprChar2 = cur.toCharArray();
+            Arrays.sort(cprChar1);
+            Arrays.sort(cprChar2);
+            if(String.valueOf(cprChar1).equals(String.valueOf(cprChar2))) {
                 list.add(cur);
                 return lastResult;
             }
         }
         List<String> curStrs = new LinkedList<>();
         curStrs.add(cur);
-        
+        lastResult.add(curStrs);
+        return lastResult;
     }
 }
