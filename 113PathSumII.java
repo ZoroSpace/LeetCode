f00cace588a80e4ac11e19fc6f8f817cd44f123a
Zoro <ZoroSpace@users.noreply.github.com>
2017-12-19 11:07:31 +0800

    Update 113PathSumII.java

--- 113PathSumII.java
+++ 113PathSumII.java
@@ -10,14 +10,17 @@
 //可能为负数
 class Solution {
     public List<List<Integer>> pathSum(TreeNode root, int sum) {
-        return helper(root,sum);
+        if(root == null) return new LinkedList<List<Integer>>();
+        List<List<Integer>> result = helper(root,sum);
+        if(result == null ) return new LinkedList<List<Integer>>();
+        return result;
     }
     
     List<List<Integer>> helper(TreeNode nodeX,int sum) {
         List<List<Integer>> result;
         if(nodeX.left == null && nodeX.right == null && (sum - nodeX.val == 0)) {
             List<Integer> vals = new LinkedList<>();
-            vals.add(nodeX.val);
+            vals.add(0,nodeX.val);
             result = new LinkedList<>();
             result.add(vals);
         } else if(nodeX.left == null && nodeX.right == null && (sum - nodeX.val != 0)) {
@@ -26,32 +29,32 @@ class Solution {
             result = helper(nodeX.left,sum - nodeX.val);
             if(result != null) {
                 for(List<Integer> l : result) {
-                    ((LinkedList<Integer>)l).add(nodeX.val);
+                    ((LinkedList<Integer>)l).add(0,nodeX.val);
                 }
             }
         } else if(nodeX.left == null && nodeX.right != null) {
             result = helper(nodeX.right,sum - nodeX.val);
             if(result != null) {
                 for(List<Integer> l : result) {
-                    ((LinkedList<Integer>)l).add(nodeX.val);
+                    ((LinkedList<Integer>)l).add(0,nodeX.val);
                 }
             }
         } else {
             List<List<Integer>> lresult = helper(nodeX.left,sum - nodeX.val);
             List<List<Integer>> Rresult = helper(nodeX.right,sum - nodeX.val);
-            result = null;
             if(lresult != null) {
                 result = new LinkedList<>();
                 for(List<Integer> l : lresult) {
-                    ((LinkedList<Integer>)l).add(nodeX.val);
-                    result.add(l);
+                    ((LinkedList<Integer>)l).add(0,nodeX.val);
                 }
-                
             }
+            result = lresult;
             if(Rresult != null) {
-                result = new LinkedList<>();
+                if(result == null) {
+                    result = new LinkedList<>();//被clear掉
+                }
                 for(List<Integer> l : Rresult) {
-                    ((LinkedList<Integer>)l).add(nodeX.val);
+                    ((LinkedList<Integer>)l).add(0,nodeX.val);
                     result.add(l);
                 }
             }
