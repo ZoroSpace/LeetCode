dfd862ba30befd9d2b528d8b79ae1db7c37a2084
Zoro <ZoroSpace@users.noreply.github.com>
2017-12-19 09:07:47 +0800

    Update 113PathSumII.java

--- 113PathSumII.java
+++ 113PathSumII.java
@@ -14,11 +14,43 @@ class Solution {
     }
     
     List<List<Integer>> helper(TreeNode nodeX,int sum) {
-        if(nodeX.left == null && nodeX.right == null && (sum - nodeX.val == 0) {
+        List<List<Integer>> result;
+        if(nodeX.left == null && nodeX.right == null && (sum - nodeX.val == 0)) {
             List<Integer> vals = new LinkedList<>();
             vals.add(nodeX.val);
-            List<List<Integer>> result = new LinkedList<>();
+            result = new LinkedList<>();
             result.add(vals);
-        } else if(nodeX.left != null && nodeX.right == null)
+        } else if(nodeX.left == null && nodeX.right == null && (sum - nodeX.val != 0)) {
+            return null;
+        } else if(nodeX.left != null && nodeX.right == null) {
+            result = helper(nodeX.left,sum - nodeX.val);
+            if(result != null) {
+                for(List<Integer> l : result) {
+                    ((LinkedList<Integer>)l).add(nodeX.val);
+                }
+            }
+        } else if(nodeX.left == null && nodeX.right != null) {
+            result = helper(nodeX.right,sum - nodeX.val);
+            if(result != null) {
+                for(List<Integer> l : result) {
+                    ((LinkedList<Integer>)l).add(nodeX.val);
+                }
+            }
+        } else {
+            result = helper(nodeX.left,sum - nodeX.val);
+            List<List<Integer>> Rresult = helper(nodeX.right,sum - nodeX.val);
+            if(result != null) {
+                for(List<Integer> l : result) {
+                    ((LinkedList<Integer>)l).add(nodeX.val);
+                }
+            }
+            if(Rresult != null) {
+                for(List<Integer> l : result) {
+                    ((LinkedList<Integer>)l).add(nodeX.val);
+                    result.add(l);
+                }
+            }
+        }
+        return result;
     }
 }
