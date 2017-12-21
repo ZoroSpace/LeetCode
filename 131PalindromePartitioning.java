61802d90a92ff8428607d4713c5ffa8085f7d896
Zoro <ZoroSpace@users.noreply.github.com>
2017-12-21 11:06:17 +0800

    Update 131PalindromePartitioning.java

--- 131PalindromePartitioning.java
+++ 131PalindromePartitioning.java
@@ -1,9 +1,8 @@
 class Solution {
     public List<List<String>> partition(String s) {
         char[] c = s.toCharArray();
-        List<List<String>> result = new LinkedList<>();
-        
-        return null;
+        // List<List<String>> result = new LinkedList<>();
+        return helper(c,0,s.length()-1);
     }
     
     boolean isPalindrome(char[] c,int lo,int hi) {
@@ -14,5 +13,22 @@ class Solution {
             } else return false;
         }
         return true;
+    }
+    
+    List<List<String>> helper(char[] c,int lo,int hi) {
+        List<List<String>> result;
+        for(int i = lo;i < hi;i++) {
+            if(isPalindrome(c,lo,i)) {
+                List<String> list = new LinkedList<>();
+                list.add(String.valueOf(copyOfRange(c,lo,i)));
+                List<List<String>> lastResult = helper(c,i+1,hi);
+                if(lastResult.size() != 0) {
+                    for(List<String> l : lastResult) {
+                        l.add(String.valueOf(copyOfRange(c,lo,i)));
+                    }
+                }
+            }
+        }
+        return lastResult;
     }
 }
