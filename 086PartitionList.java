be8bedb6bc90bc5f6c45991cbd62c54d4ca24588
Zoro <ZoroSpace@users.noreply.github.com>
2017-12-25 10:42:27 +0800

    Update 086PartitionList.java

--- 086PartitionList.java
+++ 086PartitionList.java
@@ -1,3 +1,30 @@
-/**  * Definition for singly-linked list.  * public class ListNode {  *     int val;  *     ListNode next;  *     ListNode(int x) { val = x; }  * }  */
-class Solution { 	public ListNode partition(ListNode head,int x) {  	ListNode sSource = new ListNode(-2);  	ListNode s = sSource;  	ListNode lSource = new ListNode(-1);  	ListNode l = lSource;  	while(head != null) {   	if(head.val < x) {    	s.next = head;    	l.next = null;    	s = head;   	} else {    	l.next = head;    	s.next = null;    	l = head;   	}   	head = head.next;  	}  	s.next = lSource.next;  	return sSource.next; 	}
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+	public ListNode partition(ListNode head,int x) {
+		ListNode sSource = new ListNode(-2);
+		ListNode s = sSource;
+		ListNode lSource = new ListNode(-1);
+		ListNode l = lSource;
+		while(head != null) {
+			if(head.val < x) {
+				s.next = head;
+				l.next = null;
+				s = head;
+			} else {
+				l.next = head;
+				s.next = null;
+				l = head;
+			}
+			head = head.next;
+		}
+		s.next = lSource.next;
+		return sSource.next;
+	}
 }
