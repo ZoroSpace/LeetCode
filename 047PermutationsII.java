1a3fecf882518eab75ee45cf55b899701c70b59b
Zoro <ZoroSpace@users.noreply.github.com>
2017-12-27 22:11:59 +0800

    Update 047PermutationsII.java

--- 047PermutationsII.java
+++ 047PermutationsII.java
@@ -10,22 +10,26 @@ class Solution {
         ArrayList<Integer> list2;
         int t;
         if(index == 0) {
-            list2 = new ArrayList<>(nums.length);
-            list2.set(0,nums[0]);
+            list2 = new ArrayList<>();
+            for(int i = 0;i < nums.length;i++) {
+                list2.add(nums[0]);
+            }
             newResult.add(list2);
             return newResult;
         }
         List<List<Integer>> result = helper(nums,index - 1);
         for(List<Integer> l : result) {
-            ArrayList<Integer> list = ((ArrayList<Integer>)l).clone();
+            ArrayList<Integer> list = (ArrayList<Integer>)((ArrayList<Integer>)l).clone();
             list.set(index,nums[index]);
             for(int i = 0;i < index;i++) {
-                list2 = list.clone();
-                if(nums[index] == nums[i]) continue;
-                t = list2.get(i);
-                list2.set(i,list2.get(index));
-                list2.set(index,t);
-                newResult.add(list2);
+                list2 = (ArrayList<Integer>)list.clone();
+                if(nums[index] != list2.get(i)) {
+                    t = list2.get(i);
+                    list2.set(i,list2.get(index));
+                    list2.set(index,t);
+                    newResult.add(list2);
+                }
+                    
             }
             newResult.add(list);
         }
