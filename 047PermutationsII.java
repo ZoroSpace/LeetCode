c751f4f899a9f37d3a565c6c2f53ef8fa1f4721b
Zoro <ZoroSpace@users.noreply.github.com>
2017-12-27 21:29:10 +0800

    Update and rename 47PermutationsII.java to 047PermutationsII.java

---
+++ 047PermutationsII.java
@@ -0,0 +1,34 @@
+class Solution {
+    public List<List<Integer>> permuteUnique(int[] nums) {
+        //提示：交换
+        Arrays.sort(nums);
+        return helper(nums,nums.length-1);
+    }
+    
+    List<List<Integer>> helper(int[] nums,int index) {
+        List<List<Integer>> newResult = new ArrayList<>();
+        ArrayList<Integer> list2;
+        int t;
+        if(index == 0) {
+            list2 = new ArrayList<>(nums.length);
+            list2.set(0,nums[0]);
+            newResult.add(list2);
+            return newResult;
+        }
+        List<List<Integer>> result = helper(nums,index - 1);
+        for(List<Integer> l : result) {
+            ArrayList<Integer> list = ((ArrayList<Integer>)l).clone();
+            list.set(index,nums[index]);
+            for(int i = 0;i < index;i++) {
+                list2 = list.clone();
+                if(nums[index] == nums[i]) continue;
+                t = list2.get(i);
+                list2.set(i,list2.get(index));
+                list2.set(index,t);
+                newResult.add(list2);
+            }
+            newResult.add(list);
+        }
+        return newResult;
+    }
+}
