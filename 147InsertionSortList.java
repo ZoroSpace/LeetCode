a9b2a597004f28909978b5d20f865275dbcc42ec
Zoro <ZoroSpace@users.noreply.github.com>
2017-12-28 10:52:28 +0800

    Update 147InsertionSortList.java

--- 147InsertionSortList.java
+++ 147InsertionSortList.java
@@ -8,30 +8,26 @@
  */
 class Solution {
     public ListNode insertionSortList(ListNode head) {
-//         ListNode cur = head;
-//         int size = 0;
-//         while(cur != null) {
-//             size++;
-//             cur = cur.next;
-//         }
         ListNode start1 = new ListNode(0);
         start1.next = null;
         ListNode start2 = new ListNode(0);
         start2.next = head;
+        ListNode temp;
         while(start2.next != null) {
             head = start2.next;
             start2.next = head.next;
             head.next = null;
-            while()
-            
+            temp = start1;
+            while(temp.next != null) {
+                if(head.val > temp.next.val) temp = temp.next;
+                else {
+                    head.next = temp.next;
+                    temp.next = head;
+                    break;
+                }
+            }
+            if(temp.next == null) temp.next = head;
         }
-        
-        
-        
-        
-        return start.next;
-        
+        return start1.next;
     }
-    
-    
 }
