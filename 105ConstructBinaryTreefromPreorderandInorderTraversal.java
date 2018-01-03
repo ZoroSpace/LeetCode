62b5cb9a1bdae7fb4b72d3e306b98089d0783a95
Zoro <ZoroSpace@users.noreply.github.com>
2018-01-03 12:49:15 +0800

    Update 105ConstructBinaryTreefromPreorderandInorderTraversal.java

--- 105ConstructBinaryTreefromPreorderandInorderTraversal.java
+++ 105ConstructBinaryTreefromPreorderandInorderTraversal.java
@@ -1,3 +1,32 @@
-TreeNode main(int[] pre,int[] n) {
-
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public TreeNode buildTree(int[] preorder, int[] inorder) {
+		int index = 0;
+        return helper(preorder,index,inorder,0,preorder.length-1);
+    }
+    TreeNode helper(int[] preorder,int ith,int[] inorder,int lo,int hi) {
+        if(lo > hi || lo < 0 || hi > preorder.length-1 || ith < 0 || ith > preorder.length-1) return null;
+        
+        TreeNode root = new TreeNode(preorder[ith]);
+        int i;
+        for(i = 0;i < preorder.length;i++) {
+            if(inorder[i] == preorder[ith]) break;
+        }
+        ith++;
+        TreeNode leftChild = helper(preorder,ith,inorder,lo,i-1);
+        TreeNode rightChild;
+        if(leftChild != null) ith++;
+        rightChild = helper(preorder,ith,inorder,i+1,hi);
+        root.left = leftChild;
+        root.right = rightChild;
+        return root;
+    }
 }
