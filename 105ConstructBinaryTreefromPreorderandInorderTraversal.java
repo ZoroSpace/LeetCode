3f6c9c3ad0ef134ad9c21d1c2e620dcd38cd4b6d
Zoro <ZoroSpace@users.noreply.github.com>
2018-01-03 14:57:56 +0800

    Update 105ConstructBinaryTreefromPreorderandInorderTraversal.java

--- 105ConstructBinaryTreefromPreorderandInorderTraversal.java
+++ 105ConstructBinaryTreefromPreorderandInorderTraversal.java
@@ -10,33 +10,28 @@
 class Solution {
     public TreeNode buildTree(int[] preorder, int[] inorder) {
 		int index = 0;
-        return helper(preorder,index,inorder,0,preorder.length-1);
+        return helper(preorder,inorder,index,0,preorder.length-1);
     }
-    TreeNode helper(int[] preorder,int ith,int[] inorder,int lo,int hi) {
-        if(lo > hi || lo < 0 || hi > preorder.length-1 || ith < 0 || ith > preorder.length-1) return null;
+    TreeNode helper(int[] preorder,int[] inorder,int ith,int lo,int hi) {
+        if(lo > hi || ith < 0 || ith > preorder.length-1) return null;
         
         TreeNode root = new TreeNode(preorder[ith]);
         int i;
-        for(i = 0;i < preorder.length;i++) {
+        for(i = lo;i <= hi;i++) {
             if(inorder[i] == preorder[ith]) break;
         }
-        ith++;
-        if(i != 0) {
-            int jth;
-            for(jth = 0;i < preorder.length;jth++) {
-                if(inorder[i-1] == preorder[jth]) break;
+        if(i != lo) {
+            int j;
+            for(j = lo;j <= hi;j++) {
+                if(inorder[i-1] == preorder[j]) break;
             }
-            jth++;
-            TreeNode leftChild = helper(preorder,ith,inorder,lo,i-1);
-            TreeNode rightChild = helper(preorder,jth,inorder,i+1,hi);
-            root.left = leftChild;
-            root.right = rightChild;
-            return root;
+            root.left = helper(preorder,inorder,ith+1,lo,i-1);
+            root.right = helper(preorder,inorder,j+1,i+1,hi);
         } else {
             root.left = null;
-            root.right = helper(preorder,ith+1,inorder,i+1,hi);
-            return root;
+            root.right = helper(preorder,inorder,ith+1,i+1,hi);
+            
         }
-        
+        return root;
     }
 }
