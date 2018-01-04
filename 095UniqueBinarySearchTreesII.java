f375beee644c9ac4a4acdb2ac6f6fe58655bcf8b
Zoro <ZoroSpace@users.noreply.github.com>
2018-01-04 10:32:37 +0800

    Update 095UniqueBinarySearchTreesII.java

--- 095UniqueBinarySearchTreesII.java
+++ 095UniqueBinarySearchTreesII.java
@@ -17,16 +17,16 @@ class Solution {
         TreeNode root;
         if(n == 0) {
         } else if(n == 1) {
-            root = new TreeNode(counter);
+            root = new TreeNode(hi);
             result.add(root);
         } else {
             for(int i = 0;i < n;i++) {
-                List<TreeNode> l1 = helper(i,i);
-                List<TreeNode> l2 = helper(n-1-i,n-1);
+                List<TreeNode> l1 = helper(i,lo,lo + i -1);
+                List<TreeNode> l2 = helper(n-1-i,lo+i,hi-1);
                 if(i != 0 && (n-1-i) != 0) {
                     for(TreeNode nodeX1 : l1) {
                         for(TreeNode nodeX2 : l2) {
-                            root = new TreeNode(counter);
+                            root = new TreeNode(hi);
                             root.left = nodeX1;
                             root.right = nodeX2;
                             result.add(root);
@@ -34,13 +34,13 @@ class Solution {
                     }
                 } else if(i == 0) {
                     for(TreeNode nodeX2 : l2) {
-                        root = new TreeNode(counter);
+                        root = new TreeNode(hi);
                         root.right = nodeX2;
                         result.add(root);
                     }
                 } else if((n-1-i) == 0) {
                     for(TreeNode nodeX1 : l1) {
-                        root = new TreeNode(counter);
+                        root = new TreeNode(hi);
                         root.left = nodeX1;
                         result.add(root);
                     }
