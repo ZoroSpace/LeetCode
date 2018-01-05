857e522ad964e2367300f6201248a223ca4e297e
Zoro <ZoroSpace@users.noreply.github.com>
2018-01-05 15:32:31 +0800

    Create 092ReverseLinkedListII.java

---
+++ 092ReverseLinkedListII.java
@@ -0,0 +1,27 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public ListNode reverseBetween(ListNode head, int m, int n) {
+        ListNode s1 = new ListNode(0),s2 = new ListNode(0),s0 = s1,end,cur;
+        s1.next = head;
+        for(int i = 0;i < m-1;i++) {
+            s1 = s1.next;
+        }
+        end = s1.next;
+        for(int i = m;i < n;i++) {
+            cur = s1.next;
+            s1.next = cur.next;
+            cur.next = s2.next;
+            s2.next = cur;
+        }
+        end.next = s1.next.next;
+        s1.next.next = s2.next;
+        return s0.next;
+    }
+}
