1b4f51c162b34240ad7a6b88bf09735b878c2d02
Zoro <ZoroSpace@users.noreply.github.com>
2018-01-06 15:43:10 +0800

    Update 139WordBreak.java

--- 139WordBreak.java
+++ 139WordBreak.java
@@ -1,15 +1,15 @@
 class Solution {
     public boolean wordBreak(String string, List<String> wordDict) {
         if(wordDict.size() == 0 || wordDict.get(0).length() == 0) return false;
-        return helper(string,wordDict);
-    }
-    boolean helper(String string, List<String> wordDict) {
-        if(string.length() == 0) return true;
-        for(String s : wordDict) {
-            if(string.startsWith(s)) {
-                return helper(string.substring(s.length()),wordDict);
+        boolean[] dp = new boolean[string.length()];
+        for(int i = string.length() - 1;i > -1;i--) {
+            for(String s : wordDict) {
+                if(i + s.length() < string.length()) {
+                    dp[i] = string.startsWith(s,i) && dp[i + s.length()];
+                } else dp[i] = string.startsWith(s,i);
+                if(dp[i]) break;
             }
         }
-        return false;
+        return dp[0];
     }
 }
