1bd6910289c7110b756618b5f31a743cfa1424a8
Zoro <ZoroSpace@users.noreply.github.com>
2018-01-07 13:23:35 +0800

    Update 134GasStation.java

--- 134GasStation.java
+++ 134GasStation.java
@@ -8,17 +8,16 @@ class Solution {
         }
         int left = n/2,right = n/2+1;
         while(true) {
-            while(dp[left] >= 0) {
+            while(dp[right] >= 0) {
                 right = (right+1+n)%n;
-                dp[left] = dp[left]+dp[right];
-                
-                if(left == (right+1+n) % n) {
-                    if(dp[left] >= 0) return left;
-                    else return -1;
-                }
+                dp[right] = dp[right] + dp[(right-1+n)%n];
+            }
+            dp[right]  = dp[right] + dp[left];
+            if(left == (right+1+n) % n) {
+                if(dp[left] >= 0) return left;
+                else return -1;
             }
             left = (left-1+n)%n;
-            dp[left] = dp[left] + dp[(left+1+n)%n];
         }
     }
 }
