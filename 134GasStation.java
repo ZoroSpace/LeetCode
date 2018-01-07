be24457640dcada3693ea40e7b78bfb56646108a
Zoro <ZoroSpace@users.noreply.github.com>
2018-01-07 14:45:28 +0800

    Update 134GasStation.java

--- 134GasStation.java
+++ 134GasStation.java
@@ -1,23 +1,28 @@
 class Solution {
     public int canCompleteCircuit(int[] gas, int[] cost) {
-        int n = gas.length;
-        if(n == 0) return -1;
+        int n = gas.length,start = 0,end;
         int[] dp = new int[n];
-        for(int i = 0;i < n;i++) {
-            dp[i] = gas[i] - cost[i];
-        }
-        int left = n/2,right = n/2+1;
-        while(true) {
-            while(dp[right] >= 0) {
-                right = (right+1+n)%n;
-                dp[right] = dp[right] + dp[(right-1+n)%n];
+        for(int i = 0;i < n;i++) dp[i] = gas[i] - cost[i];
+        if(n == 0) return -1;
+        for(int i = 0;i < n; i++) {
+            if(dp[i] >= 0) {
+                start = i;
+                break;
             }
-            dp[right]  = dp[right] + dp[left];
-            if(left == (right+1+n) % n) {
-                if(dp[left] >= 0) return left;
-                else return -1;
-            }
-            left = (left-1+n)%n;
         }
+        if(dp[start] < 0) return -1;
+        end = (start+1+n)%n;
+        dp[end] = dp[end] + dp[start];
+        while(start != end) {
+            if(dp[end] >= 0) {
+                end = (end+1+n)%n;
+                dp[end] = dp[end] + dp[(end-1+n)%n];
+            } else {
+                start = (start-1+n)%n;
+                dp[end] = dp[end] + dp[start];
+            }
+        }
+        if(dp[start] >= 0) return start;
+        else return -1;
     }
 }
