b8f2e209a08196eeaaf759ebbd145364eee35d73
Zoro <ZoroSpace@users.noreply.github.com>
2018-01-07 13:09:01 +0800

    Create 134GasStation.java

---
+++ 134GasStation.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int canCompleteCircuit(int[] gas, int[] cost) {
+        int n = gas.length;
+        if(n == 0) return -1;
+        int[] dp = new int[n];
+        for(int i = 0;i < n;i++) {
+            dp[i] = gas[i] - cost[i];
+        }
+        int left = n/2,right = n/2+1;
+        while(true) {
+            while(dp[left] >= 0) {
+                right = (right+1+n)%n;
+                dp[left] = dp[left]+dp[right];
+                
+                if(left == (right+1+n) % n) {
+                    if(dp[left] >= 0) return left;
+                    else return -1;
+                }
+            }
+            left = (left-1+n)%n;
+            dp[left] = dp[left] + dp[(left+1+n)%n];
+        }
+    }
+}
