21db2dcbfbba3bd50de189c16a1a04623a25ecde
Zoro <ZoroSpace@users.noreply.github.com>
2018-01-10 22:35:24 +0800

    Create 043MultiplyStrings.java

---
+++ 043MultiplyStrings.java
@@ -0,0 +1,16 @@
+class Solution {
+    public String multiply(String num1, String num2) {
+        //要考虑其中一个是0的情况
+        int n1 = num1.length();
+        int n2 = num2.length();
+        char[] cs1 = num1.toCharArray();
+        char[] cs2 = num2.toCharArray();
+        int[] result = new int[n1 + n2 -1];
+        for(int i = n1 + n2 -2;i > -1;i--) {
+            for(int j = n2 - 1;j > -1;j--) {
+                
+            }
+        }
+        return null;
+    }
+}
