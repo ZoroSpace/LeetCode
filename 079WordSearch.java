1f12c5bc534edfd2e37af08c73d6c17362e23450
Zoro <ZoroSpace@users.noreply.github.com>
2018-01-11 11:25:54 +0800

    Create 079WordSearch.java

---
+++ 079WordSearch.java
@@ -0,0 +1,6 @@
+class Solution {
+    public boolean exist(char[][] board, String word) {
+        if(word.length() == 0 || board.length == 0 || board[0].length == 0) return false;
+        return true;
+    }
+}
