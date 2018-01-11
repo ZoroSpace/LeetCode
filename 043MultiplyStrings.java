2e8f8d75893c2884393e7af1bc83964a6e047677
Zoro <ZoroSpace@users.noreply.github.com>
2018-01-11 11:12:21 +0800

    Update 043MultiplyStrings.java

--- 043MultiplyStrings.java
+++ 043MultiplyStrings.java
@@ -6,16 +6,17 @@ class Solution {
         char[] cs2 = num2.toCharArray();
         if(cs1[0] == '0' || cs2[0] == '0') return "0";//要考虑其中一个是0的情况
         int[] result = new int[n1 + n2 -1];
-        for(int i = n1 + n2 -2;i > -1;i--) {
-            for(int j = n2 - 1;0 <= i-j && i-j <= n1-1;j--) {
-                result[i] = (cs2[j] - '0') * (cs1[i-j] - '0');
+        for(int i = 0;i < n1+n2-1;i++) {
+            result[i] = 0;
+            for(int j = Math.max(0,i+1-n2);j <= i && j < n1;j++) {
+                result[i] = result[i] + (cs1[j] - 48) * (cs2[i-j] - 48);
             }
         }
         int c = 0;
         StringBuilder sb = new StringBuilder("");
         for(int i = n1 + n2 - 2;i > -1;i--) {
-            sb.append(c+result[i] % 10);
-            c = result[i]/10;
+            sb.append((c+result[i]) % 10);
+            c = (c+result[i])/10;
         }
         if(c != 0) sb.append(c);
         return sb.reverse().toString();
