6647e0354f4d4737d41e8c7aafef5bccb51e710f
Zoro <ZoroSpace@users.noreply.github.com>
2018-01-16 20:09:53 +0800

    Update 005LongestPalindromicSubstring.java

--- 005LongestPalindromicSubstring.java
+++ 005LongestPalindromicSubstring.java
@@ -1,33 +1,42 @@
 class Solution {
     public String longestPalindrome(String s) {
+        if(s.length() == 0) return s;
         s = "*" + s;
-        int[][] dp = new int[2][s.length()];//用于记录dp[1][i]和i之间为回文,inclusive;和回文字符串的长度
+        ArrayList<Integer>[] match = new ArrayList[s.length()];//用于记录第i项和第ArrayList里的项之间构成回文序列
+        for(int i=0;i<match.length;i++) {
+            match[i]=new ArrayList<Integer>();
+        }
+        int[] span = new int[s.length()];//用于记录第i项构成回文序列的最长长度
+        
+        match[1].add(1);//初始化
+        span[1] = 0;
         int longest = 0;
+        
         boolean flag = false;
-        dp[0][1] = 1;
-        dp[1][1] = 0;
         char cur;
-        for(int i = 2;i < dp[0].length;i++) {
-            //计算dp[0]
+        
+        for(int i = 2;i < s.length();i++) {
+            //计算match
             cur = s.charAt(i);
-            
-            if(cur == s.charAt(dp[0][i-1]-1)) dp[0][i] = dp[0][i-1]-1;
-            else {
-                if(cur != s.charAt(i-1))  {
-                    dp[0][i] = i;
-                } else {
-                    dp[0][i] = flag ? dp[0][i-1] : i-1;
-                }
+            for(int j : match[i-1]) {
+                if(cur == s.charAt(j - 1)) match[i].add(j - 1);
+            }
+            if(cur != s.charAt(i-1))  {
+                match[i].add(i);
+            } else {
+                if(!flag) match[i].add(i-1);
+                else match[i].add(match[i-1].get(0));
             }
             flag = cur == s.charAt(i-1) ? true:false;
-            //计算dp[1]
-            dp[1][i] = i - dp[0][i];
-            longest = Math.max(longest,dp[1][i]);
+            
+            //span
+            span[i] = i - match[i].get(0);
+            longest = Math.max(longest,span[i]);
         }
         int i;
-        for(i = 1;i < dp[0].length;i++) {
-            if(dp[1][i] == longest) break;
+        for(i = 1;i < span.length;i++) {
+            if(span[i] == longest) break;
         }
-        return s.substring(dp[0][i],i+1);
+        return s.substring(match[i].get(0),i+1);
     }
 }
