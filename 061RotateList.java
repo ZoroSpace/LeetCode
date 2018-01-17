971b19a9f773ce996e7fd44b089deb1fae323fa5
Zoro <ZoroSpace@users.noreply.github.com>
2018-01-17 10:41:30 +0800

    Update 061RotateList.java

--- 061RotateList.java
+++ 061RotateList.java
@@ -8,20 +8,22 @@
  */
 class Solution {
     public ListNode rotateRight(ListNode head, int k) {
-        //若k很大
-        ListNode s1 = new ListNode(0);
-        ListNode s2 = new ListNode(0);
-        s1.next = head;
-        for(int i = 0;i < k-1;i++) {
-            head = head.next;
+        if(k < 0 || head == null) return null;
+        int length = 1;
+        ListNode cur = head;
+        while(cur.next != null) {
+            cur = cur.next;
+            length++;
         }
-        if(head.next == null) return s1.next;
-        s2.next = head.next;
-        head.next = null;
-        head = s2.next;
-        while(head.next != null) head = head.next;
-        head.next = s1.next;
-        return s2.next;
-        
+        k = k%length;
+        if(k == 0) return head;
+        k = length - k;
+        cur.next = head;
+        for(int i = 0;i < k;i++) {
+            cur = cur.next;
+        }
+        head = cur.next;
+        cur.next = null;
+        return head;
     }
 }
