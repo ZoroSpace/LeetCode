2ae66f9e13bbb52d4e28f6e2fde1c3d61c6af579
Zoro <ZoroSpace@users.noreply.github.com>
2018-01-17 09:46:37 +0800

    Update 003LongestSubstringWithoutRepeatingCharacters.java

--- 003LongestSubstringWithoutRepeatingCharacters.java
+++ 003LongestSubstringWithoutRepeatingCharacters.java
@@ -1,34 +1,17 @@
 class Solution {
     public int lengthOfLongestSubstring(String s) {
         if(s.length() == 0) return 0;
-        int[] dp = new int[s.length()];
         int result = 1;
-        boolean flag = false;
         int lo = 0,hi = 1;
         while(hi < s.length()) {
-            if(s.charAt(hi) == s.charAt(hi - 1)) {
-                flag = true;
-                hi++;
-            } else if(flag) {
-                lo = hi - 1;
-                dp[hi] = 2;
-                result = Math.max(result,dp[hi]);
-                flag = false;
-                hi++;
-            } else {
-                if(s.charAt(lo) == s.charAt(hi)) {
-                    lo++;
-                    dp[hi] = hi-lo+1;
-                    result = Math.max(result,dp[hi]);
-                    hi++;
-                    flag = false;
-                } else {
-                    dp[hi] = hi-lo+1;
-                    result = Math.max(result,dp[hi]);
-                    hi++;
-                    flag = false;
+            for(int i = hi - 1;i >= lo;i--) {
+                if(s.charAt(hi) == s.charAt(i)) {
+                    lo = i + 1;
+                    break;
                 }
             }
+            result = Math.max(result,hi-lo+1);
+            hi++;
         }
         return result;
     }
