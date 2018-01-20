c4bcd2a2b2bf055716f5e470a90d443280bf3fe6
Zoro <ZoroSpace@users.noreply.github.com>
2018-01-20 17:34:16 +0800

    Update 127WordLadder.java

--- 127WordLadder.java
+++ 127WordLadder.java
@@ -8,36 +8,39 @@ class Solution {
             }
         }
         if(!flag) return 0;
-        List<String> curStep = new LinkedList<>();
-        List<String> nextStep = wordList;
-        curStep.add(beginWord);
+        
+        HashMap<String,Boolean> visited = new HashMap<>();
+        for(String s : wordList) visited.put(s,false);
+        
         int result = 2;
-        List<String> curTemp = new LinkedList<>();
-        List<String> nextTemp = new LinkedList<>();
-        while(curStep.size() != 0) {
-            curTemp = new LinkedList<>();
-            nextTemp = new LinkedList<>();
-            for(String s1 : curStep) {
-                if(helper(s1,endWord)) return result;
-                for(String s2 : nextStep) {
-                    if(helper(s1,s2)) curTemp.add(s2);
-                    else nextTemp.add(s2);
+        
+        LinkedList<String> list = new LinkedList<>();
+        list.offer(beginWord);
+        
+        int size;
+        String cur;
+        String changed;
+        char[] curCharArray;
+        while(list.size() != 0) {
+            size = list.size();
+            for(int i = 0;i < size;i++) {
+                cur = list.poll();
+                for(int j = 0;j < cur.length();j++) {
+                    for(char c = 'a';c <= 'z';c++) {
+                        if(c == cur.charAt(j)) continue;
+                        curCharArray = cur.toCharArray();
+                        curCharArray[j] = c;
+                        changed = String.valueOf(curCharArray);
+                        if(changed.equals(endWord)) return result;
+                        if(visited.containsKey(changed) && !visited.get(changed)) {
+                            visited.put(changed,true);
+                            list.offer(changed);
+                        }
+                    }
                 }
             }
-            curStep = curTemp;
-            nextStep = nextTemp;
             result++;
         }
         return 0;
-    }
-    
-    boolean helper(String s1,String s2) {
-        //检查s1和s2的“距离”是不是1
-        int counter = 0;
-        for(int i = 0;i < s1.length();i++) {
-            if(s1.charAt(i) != s2.charAt(i)) counter++;
-            if(counter == 2) return false;
-        }
-        return counter == 1;
     }
 }
