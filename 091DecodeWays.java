ea42788a2ccc685d8e88c896a815aacfcc0094f4
Zoro <ZoroSpace@users.noreply.github.com>
2018-01-20 09:41:35 +0800

    Update 091DecodeWays.java

--- 091DecodeWays.java
+++ 091DecodeWays.java
@@ -1,1 +1,15 @@
-class Solution {}
+class Solution {
+    public int numDecodings(String s) {
+        if(s.length() == 0 || s.charAt(0) == '0') return 0;
+        int[] dp = new int[s.length()+1];
+        dp[0] = 1;
+        dp[1] = 1;
+        for(int i = 2;i < dp.length;i++) {
+            if(s.charAt(i-1) == '0' && s.charAt(i-2) != '2'  && s.charAt(i-2) != '1') return 0;
+            if(s.charAt(i-1) == '0') dp[i] = dp[i-2];
+            else if(s.charAt(i-2) == '1'||(s.charAt(i-2) == '2' && s.charAt(i-1) <= '6')) dp[i] = dp[i-1] + dp[i-2];
+            else dp[i] = dp[i-1];
+        }
+        return dp[s.length()];
+    }
+}
