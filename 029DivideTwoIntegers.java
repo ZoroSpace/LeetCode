dc8782b43bbde43a537251f862b8d1c61b98dbc0
Zoro <ZoroSpace@users.noreply.github.com>
2018-01-22 16:44:39 +0800

    Update 029DivideTwoIntegers.java

--- 029DivideTwoIntegers.java
+++ 029DivideTwoIntegers.java
@@ -5,15 +5,33 @@ class Solution {
         if(dividend == Integer.MIN_VALUE && divisor == Integer.MIN_VALUE) return 1;
         if(divisor == Integer.MIN_VALUE) return 0;
         boolean flag = true;
-        if(dividend < 0 && divisor < 0) flag = true;
-        if(dividend > 0 && divisor < 0) flag = false;
-        if(dividend < 0 && divisor > 0) flag = false;
+        if(dividend > 0 && divisor > 0) {
+            if(dividend < divisor) return 0;
+            flag = true;
+        }
+        if(dividend < 0 && divisor < 0) {
+            if(dividend > divisor) return 0;
+            flag = true;
+        }
+        if(dividend > 0 && divisor < 0) {
+            if(-dividend > divisor) return 0;
+            flag = false;
+        }
+        if(dividend < 0 && divisor > 0) {
+            if(dividend > -divisor) return 0;
+            flag =false;
+        }
 
         StringBuilder result = new StringBuilder("");
         String dividendString = String.valueOf(dividend);
         String divisorString = String.valueOf(divisor);
-        if(dividend < 0) dividendString = dividendString.substring(1);
-        if(divisor < 0) divisorString = divisorString.substring(1);
+        if(dividend < 0) {
+            dividendString = dividendString.substring(1);
+        }
+        if(divisor < 0) {
+            divisorString = divisorString.substring(1);
+            divisor = Integer.parseInt(divisorString);
+        }
         int counter = 0;
         char last = '0';
         StringBuilder numString = new StringBuilder("");
@@ -30,6 +48,7 @@ class Solution {
             n1 = Integer.parseInt(numString.toString());
             counter = 0;
         }
+        if(flag && result.toString().equals("2147483648")) return 2147483647;
         if(flag) return Integer.parseInt(result.toString());
         else return Integer.parseInt("-"+result.toString());
     }
