dfa284ec7df2f3f648e871f61b281723f7154a7b
Zoro <ZoroSpace@users.noreply.github.com>
2018-01-23 16:52:47 +0800

    Update 052N-QueensII.java

--- 052N-QueensII.java
+++ 052N-QueensII.java
@@ -36,3 +36,42 @@ class Solution {
         return counter;
     }
 }
+
+
+class Solution {
+    public int totalNQueens(int n) {
+        //test case : n  <= 13
+        if(n <= 1) return 1;
+        int[] a = new int[n];
+        // return helper(a,1,0);
+        int counter = 0;
+        for(int i = 0;i < n/2;i++) {
+            a[0] = i;
+            counter = counter+helper(a,2,0);
+            a[0] = -1;
+        }
+        if(n%2 == 0) return 2*counter;
+        else {
+            a[0] = n/2;
+            return counter*2+helper(a,2,0);
+        }
+    }
+    
+    int helper(int[] a,int level,int counter) {
+        for(int i = 0;i < a.length;i++) {
+            a[level-1] = i;
+            if(isValid(a,level)) {
+                if(level == a.length) return counter+1;
+                counter = helper(a,level+1,counter);
+            }
+        }
+        return counter;
+    }
+    boolean isValid(int[] a,int level) {
+        for(int i = 0;i< level-1;i++) {
+            if(a[i] == a[level-1] || (Math.abs(a[i] - a[level-1])==Math.abs(i-level+1)))
+                return false;
+        }
+        return true;
+    }
+}
