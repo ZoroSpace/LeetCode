17b7a5deb2a04433523ea06766cda933cd569ff8
Zoro <ZoroSpace@users.noreply.github.com>
2018-01-23 15:17:44 +0800

    Update 052N-QueensII.java

--- 052N-QueensII.java
+++ 052N-QueensII.java
@@ -4,7 +4,17 @@ class Solution {
         if(n <= 1) return 1;
         int[] a = new int[n];
         for(int i = 0;i < n;i++) a[i] = -1;
-        return helper(a,0,0);
+        int counter = 0;
+        for(int i = 0;i < n/2;i++) {
+            a[0] = i;
+            counter = counter+helper(a,1,0);
+            a[0] = -1;
+        }
+        if(n%2 == 0) return 2*counter;
+        else {
+            a[0] = n/2;
+            return counter*2+helper(a,1,0);
+        }
     }
     
     int helper(int[] a,int level,int counter) {
@@ -16,11 +26,11 @@ class Solution {
             set.remove(a[i]+level-i);
         }
         if(set.isEmpty()) return counter;
-        else if(set.size() == 1 && level == a.length-1) return 1; 
+        else if(set.size() == 1 && level == a.length-1) return counter+1; 
         else {
             for(int i : set) {
                 a[level] = i;
-                counter = counter+helper(a,level+1,counter);
+                counter = helper(a,level+1,counter);
             }
         }
         return counter;
