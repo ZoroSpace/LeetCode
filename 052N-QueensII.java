db5b313ff9a4b93e33df71e5cc5339724f531e5d
Zoro <ZoroSpace@users.noreply.github.com>
2018-01-25 09:08:45 +0800

    Update 052N-QueensII.java

--- 052N-QueensII.java
+++ 052N-QueensII.java
@@ -75,3 +75,25 @@ class Solution {
         return true;
     }
 }
+
+class Solution {
+    private int counter;
+    public int totalNQueens(int n) {
+        counter = 0;
+        if(n <= 1) return 1;
+        helper(new int[n],new boolean[n],new boolean[2*n],new boolean[2*n],0,n);
+        return counter;
+    }
+    void helper(int[] p,boolean[] a,boolean[] aPlus,boolean[] aMinus,int level,int n) {
+        for(int i = 0;i < n;i++) {
+            if(a[i] || aPlus[i+level] || aMinus[i-level+n-1]) continue;
+            else {
+                p[level] = i;
+                if(level == n-1) counter++;
+                a[i] = true;aPlus[i+level] = true;aMinus[i-level+n-1] = true;
+                helper(p,a,aPlus,aMinus,level+1,n);
+                a[i] = false;aPlus[i+level] = false;aMinus[i-level+n-1] = false;
+            }
+        }
+    }
+}
