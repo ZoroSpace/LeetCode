5e2ad6764e9dcfea112222d5426a4e3671dbec1c
Zoro <ZoroSpace@users.noreply.github.com>
2018-02-04 21:49:10 +0800

    Update 087ScrambleString.java

--- 087ScrambleString.java
+++ 087ScrambleString.java
@@ -1,6 +1,6 @@
 class Solution {
     public boolean isScramble(String s1, String s2) {
-        if(s1.length() != s2.length) return false;
+        if(s1.length() != s2.length()) return false;
         int[] dic = new int[26];
         for(int i = 0;i < s1.length();i++) {
             dic[s1.charAt(i)-'a']++;
