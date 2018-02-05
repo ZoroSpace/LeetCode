634c111d9e414f203a204db4f830f648c0945620
Zoro <ZoroSpace@users.noreply.github.com>
2018-02-05 09:36:45 +0800

    Update 087ScrambleString.java

--- 087ScrambleString.java
+++ 087ScrambleString.java
@@ -1,14 +1,6 @@
 class Solution {
     public boolean isScramble(String s1, String s2) {
         if(s1.length() != s2.length()) return false;
-        int[] dic = new int[26];
-        for(int i = 0;i < s1.length();i++) {
-            dic[s1.charAt(i)-'a']++;
-            dic[s2.charAt(i)-'a']--;
-        }
-        for(int i = 0;i < 26;i++) {
-            if(dic[i] != 0) return false;
-        }
         return helper(s1,s2) || helper(s1,new StringBuffer(s2).reverse().toString());
     }
         
