1c45701193e00e4e7148b2a82e4fb45194e6011e
Zoro <ZoroSpace@users.noreply.github.com>
2018-02-05 09:36:09 +0800

    Update 087ScrambleString.java

--- 087ScrambleString.java
+++ 087ScrambleString.java
@@ -14,14 +14,20 @@ class Solution {
         
     boolean helper(String s1,String s2) {
         if(s1.equals(s2)) return true;
+        int[] dic = new int[26];
+        for(int i = 0;i < s1.length();i++) {
+            dic[s1.charAt(i)-'a']++;
+            dic[s2.charAt(i)-'a']--;
+        }
+        for(int i = 0;i < 26;i++) {
+            if(dic[i] != 0) return false;
+        }
         String s3,s4,s5,s6;
         boolean flag = false;
         for(int i = 1;i < s1.length();i++) {
-            s3 = s1.substring(0,i);
-            s4 = s2.substring(0,i);
-            s5 = s1.substring(i,s1.length());
-            s6 = s2.substring(i,s1.length());
-            flag = (helper(s3,s4) || helper(s3,new StringBuffer(s4).reverse().toString()))&&(helper(s5,s6) || helper(s5,new StringBuffer(s6).reverse().toString()));
+            flag = helper(s1.substring(0,i),s2.substring(0,i)) && helper(s1.substring(i,s1.length()),s2.substring(i,s1.length()));
+            if(flag) return true;
+            flag = helper(s1.substring(0,i),s2.substring(s1.length()-i,s1.length())) && helper(s1.substring(i,s1.length()),s2.substring(0,s1.length()-i));
             if(flag) return true;
         }
         return false;
