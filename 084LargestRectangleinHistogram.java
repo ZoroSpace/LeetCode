92dfb67fbf4d3a2782028e700803f0d2cf9830a5
Zoro <ZoroSpace@users.noreply.github.com>
2018-02-08 08:32:51 +0800

    Update 084LargestRectangleinHistogram.java

--- 084LargestRectangleinHistogram.java
+++ 084LargestRectangleinHistogram.java
@@ -21,3 +21,45 @@ class Solution {
         return result;
     }
 }
+
+
+class Solution {
+    public int largestRectangleArea(int[] a) {
+        if(a.length == 0) return 0;
+        int n = a.length;
+        int[] left = new int[n],right = new int[n];
+        left[0] = 0;
+        right[n-1] = n-1;
+        int result = 0;
+        Stack<Integer> stack = new Stack<>();
+        stack.push(0);
+        int j;
+        for(int i = 1;i < n;i++) {
+            if(a[i] > a[i-1]) {
+                left[i] = i;
+            } else if(a[i] == a[i-1]) left[i] = left[i-1];
+            else {
+                while(stack.size() != 0 && a[stack.peek()] >= a[i]) stack.pop();
+                if(stack.size() == 0) left[i] = 0;
+                else left[i] = stack.peek()+1;
+            }
+                stack.push(i);                
+        }
+        stack.clear();
+        stack.push(n-1);
+        for(int i = n-2;i >= 0;i--) {
+            if(a[i] > a[i+1]) right[i] = i;
+            else if(a[i] == a[i+1]) right[i] = right[i+1];
+            else {
+                while(stack.size() != 0 && a[stack.peek()] >= a[i]) stack.pop();
+                if(stack.size() == 0) right[i] = n-1;
+                else right[i] = stack.peek()-1;
+            }
+            stack.push(i);
+        }
+        for(int i = 0;i < n;i++) {
+            result = Math.max(result,(right[i]-left[i]+1)*a[i]);
+        }
+        return result;
+    }
+}
