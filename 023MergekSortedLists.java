190e0ab3aa39140fb33739e859ae01a199c5fe5e
Zoro <ZoroSpace@users.noreply.github.com>
2018-02-08 16:15:15 +0800

    Update 023MergekSortedLists.java

--- 023MergekSortedLists.java
+++ 023MergekSortedLists.java
@@ -35,3 +35,41 @@ class Solution {
     }
     
 }
+
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public ListNode mergeKLists(ListNode[] lists) {
+        if(lists == null || lists.length == 0) return null;
+        return helper(lists,0,lists.length - 1);
+    }
+    
+    ListNode helper(ListNode[] lists,int lo,int hi) {
+        if(lo == hi) return lists[lo];
+        ListNode s1 = helper(lists,lo,(lo+hi)/2);
+        ListNode s2 = helper(lists,(lo+hi)/2+1,hi);
+        ListNode start = new ListNode(0),end = start;
+        while(s1 != null && s2 != null) {
+            if(s1.val < s2.val) {
+                end.next = s1;
+                s1 = s1.next;
+            } else {
+                end.next = s2;
+                s2 = s2.next;
+            }
+            end = end.next;
+            end.next = null;
+        }
+        if(s1 != null) end.next = s1;
+        if(s2 != null) end.next = s2;
+        return start.next;
+    }
+    
+}
