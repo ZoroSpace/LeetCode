c87c00cf7ebbe79ecc5567adc117c55ddfe9ab54
Zoro <ZoroSpace@users.noreply.github.com>
2018-02-10 18:18:39 +0800

    Update 041FirstMissingPositive.java

--- 041FirstMissingPositive.java
+++ 041FirstMissingPositive.java
@@ -12,3 +12,22 @@ class Solution {
         return n+1;
     }
 }
+
+
+class Solution {
+    public int firstMissingPositive(int[] nums) {
+        int n = nums.length,t;
+        for(int i = 0;i < n;i++) {
+            while(nums[i] >= 1 && nums[i] <= n && nums[nums[i]-1] != nums[i]) {
+                t = nums[i];
+                nums[i] = nums[t-1];
+                nums[t-1] = t;
+            }
+        }
+        for(int i = 0;i < n;i++) {
+            if(nums[i] != i+1) return i+1;
+        }
+        return n+1;
+    }
+    
+}
