f25be81f90a063fa06360ff1f8dc3e87b8b48bb3
Zoro <ZoroSpace@users.noreply.github.com>
2018-02-11 10:42:11 +0800

    Create 135Candy.java

---
+++ 135Candy.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int candy(int[] ratings) {
+        int n = ratings.length;
+        if(n <= 1) return n;
+        boolean[] L = new boolean[n];
+        boolean[] R = new boolean[n];
+        for(int i = 0;i < n-1;i++) {
+            if(ratings[i] < ratings[i+1]) L[i+1] = true;
+            if(ratings[i] > ratings[i+1]) R[i] = true;
+        }
+        int[] c = new int[n];
+        c[0] = 1;
+        for(int i = 1;i < n;i++) {
+            if(L[i]) c[i] = c[i-1]+1;
+            else c[i] = 1;
+        }
+        for(int i = n-2;i > -1;i--) {
+            if(R[i]) c[i] = Math.max(c[i],c[i+1]+1);
+        }
+        int result = 0;
+        for(int i = 0;i < n;i++) {
+            result += c[i];
+        }
+        return result;
+    }
+}
