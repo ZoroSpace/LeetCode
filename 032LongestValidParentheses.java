d320bb6578ae11c08169340b1efddacb3d4ce7ca
Zoro <ZoroSpace@users.noreply.github.com>
2018-02-19 20:03:22 +0800

    Update 032LongestValidParentheses.java

--- 032LongestValidParentheses.java
+++ 032LongestValidParentheses.java
@@ -100,3 +100,23 @@ class Solution {
         return result;
     }
 }
+
+
+class Solution {
+    public int longestValidParentheses(String s) {
+        int n = s.length();
+        int[] dp = new int[n];
+        int result = 0;
+        for(int i = 1;i < n;i++) {
+            if(s.charAt(i) == ')') {
+                if(s.charAt(i-1) == '(') {
+                    dp[i] = 2 + (i > 1 ? dp[i-2] : 0); 
+                } else if(i > dp[i-1] && s.charAt(i-1-dp[i-1]) == '(') {
+                    dp[i] = dp[i-1] + 2 + (i-1-dp[i-1] > 0 ? dp[i-2-dp[i-1]] : 0);
+                }
+                result = Math.max(result,dp[i]);
+            }
+        }
+        return result;
+    }
+}
