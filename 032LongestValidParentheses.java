f9965daa52a874e62e98c4b784a7a1a1d0116cb1
Zoro <ZoroSpace@users.noreply.github.com>
2018-02-19 19:39:33 +0800

    Update 032LongestValidParentheses.java

--- 032LongestValidParentheses.java
+++ 032LongestValidParentheses.java
@@ -62,3 +62,41 @@ class Solution {
         return result;
     }
 }
+
+
+class Solution {
+    public int longestValidParentheses(String s) {
+        int n = s.length();
+        if(n <= 1) return 0;
+        Stack<Integer> stack = new Stack<>();
+        int result = 0;
+        int counter = 0;
+        for(int i = 0;i < n;i++) {
+            counter = 0;
+            if(!(stack.empty() || stack.peek() == -1 || stack.peek() == -2)) {
+                counter = stack.pop();
+            }
+            if(s.charAt(i) == '(') {
+                if(counter != 0) stack.push(counter);
+                stack.push(-1);
+            } else {
+                if(!stack.empty() && stack.peek() == -1) {
+                    stack.pop();
+                    stack.push(counter+2);
+                } else {
+                    if(counter != 0) stack.push(counter);
+                    stack.push(-2);
+                }
+            }
+            counter = 0;
+            while(!(stack.empty() || stack.peek() == -1 || stack.peek() == -2)) {
+                counter += stack.pop();
+            }
+            if(counter != 0) {
+                stack.push(counter);
+                result = Math.max(result,counter);
+            }
+        }
+        return result;
+    }
+}
