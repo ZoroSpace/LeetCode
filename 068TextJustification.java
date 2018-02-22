e32bc870edf51c5029ee7fd1b9eb5e8f46c87484
Zoro <ZoroSpace@users.noreply.github.com>
2018-02-22 23:00:55 +0800

    Update 068TextJustification.java

--- 068TextJustification.java
+++ 068TextJustification.java
@@ -2,37 +2,32 @@ class Solution {
     public List<String> fullJustify(String[] words, int maxWidth) {
         LinkedList<String> result = new LinkedList<>();
         LinkedList<String> list = new LinkedList<>();
-        for(int i = 0;i < words.length;i++) {
-            list.offerLast(words[i]);
+        int cur = 0,counter = 0;
+        String begin = words[cur];
+        counter = begin.length();
+        list.offerFirst(begin);
+        while(cur < words.length) {
+            if(counter <= maxWidth) {
+                cur++;
+                if(cur > words.length - 1) break;
+                counter = counter + 1 + words[cur].length();
+                list.offerLast(words[cur]);
+            } else {
+                list.pollLast();
+                result.offerLast(helper1(list,maxWidth));
+                begin = words[cur];
+                counter = begin.length();
+                list.offerFirst(begin);
+            }
+                
         }
-        result.add(helper1(list,16));
+        if(cur >= words.length - 1) result.offerLast(helper2(list,maxWidth));        
         return result;
-        
-        
-//         int counter;
-//         int slow = 0,fast;
-//         String begin = words[slow];
-//         counter = begin.length();
-//         list.offerFirst(begin);
-//         for(fast = slow + 1;fast < words.length;fast++) {
-//             counter = counter + 1 + words[fast].length();
-//             list.offerFirst(fast);
-//             if(counter > maxWidth) break;//也可能因为单词都添加完而执行完这段代码
-//         }
-//         list.pollLast();
-//         counter = counter - words[fast] - 1;
-//         int allSpace = maxWidth;
-//         for(String s : list) {
-//             allSpace = allSpace - s.length();
-//         }
-        
-//         for(int i = 0;i < words.length;i++) {
-            
-//         }
     }
     
     String helper1(LinkedList<String> list,int maxWidth) {
         //对于非最后一行
+        if(list.peekFirst().length() == maxWidth) return list.pollFirst();
         StringBuilder sb = new StringBuilder("");
         int allSpace = maxWidth;
         for(String s : list) allSpace = allSpace - s.length();
@@ -52,10 +47,12 @@ class Solution {
                 sb.append(spacesb).append(list.pollFirst());
             }
         }
+        list.clear();
         return sb.toString();
     }
     String helper2(LinkedList<String> list,int maxWidth) {
         //对于最后一行
+        if(list.peekFirst().length() == maxWidth) return list.pollFirst();
         StringBuilder sb = new StringBuilder("");
         int size = list.size();
         for(int i = 0;i < size;i++) {
@@ -63,6 +60,7 @@ class Solution {
             maxWidth = maxWidth - 1 - list.pollFirst().length();
         }
         for(int i = 0;i < maxWidth;i++) sb.append(" ");
+        list.clear();
         return sb.toString();
     }
 }
