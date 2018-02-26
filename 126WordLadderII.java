2a279ac54d962f5640aced7dfff616bd1b3170c1
Zoro <ZoroSpace@users.noreply.github.com>
2018-02-26 10:10:01 +0800

    Update 126WordLadderII.java

--- 126WordLadderII.java
+++ 126WordLadderII.java
@@ -1,52 +1,43 @@
 public class Solution {
-	List<List<String>> result;
-	int step;
-	public List<List<String>> findLadders(String beginWord,String endWord,List<String> dict) {
-		step = dict.size()+1;
-		result = new LinkedList<>();
-		//if(!dict.contains(endWord)) return result;
-		HashMap<String,Boolean> map = new HashMap<>();
-		for(String s : dict) {
-			map.put(s,false);
-		}
-		int counter = 0;
-		if(dict.contains(beginWord)) {
-			map.put(beginWord,true);
-			counter++;
-		}
-		LinkedList<String> list = new LinkedList<>();
-		list.offerFirst(beginWord);
-		dfs(beginWord,endWord,dict,map,list,counter);
-		LinkedList<List<String>> r = new LinkedList<>();
-		for(List<String> l : result) {
-			if(l.size()==step) {
-				r.add(l);
-			}
-		}
-		return r;
-	}
-	
-	void dfs(String beginW,String endW,List<String> dict,HashMap<String,Boolean> map,LinkedList<String> list,int counter) {
-		if(counter > dict.size()) return;
-		for(int i = 0;i < beginW.length();i++) {
-			for(char j = 'a';j <= 'z';j++) {
-				if(j != beginW.charAt(i)) {
-					String s = beginW.substring(0,i)+ j + beginW.substring(i+1,beginW.length());
-					if(map.containsKey(s)&&!map.get(s)) {
-						map.put(s,true);
-						LinkedList<String> cur =  (LinkedList<String>)(list.clone());
-						cur.offerLast(s);
-						if(s.equals(endW)) {
-							result.add(cur);
-							step = Math.min(step,cur.size());
-							map.put(s,false);
-							return;
-						}
-						dfs(s,endW,dict,map,cur,counter + 1);
-						map.put(s,false);
-					}
-				}
-			}
-		}
-	}
+    public List<List<String>> findLadders(String beginWord,String endWord,List<String> dict) {
+        List<List<String>> result = new LinkedList<>();
+        if(!dict.contains(endWord)) return result;
+        Queue<LinkedList<String>> q = new LinkedList<>();
+        LinkedList<String> elem = new LinkedList<>();
+        HashSet<String> set = new HashSet<String>(dict);
+        elem.offerFirst(beginWord);
+        q.offer(elem);
+        int size = q.size();
+        LinkedList<String> cur;
+        String last;
+        String change;
+        LinkedList plus;
+        boolean found = false;
+        //bfs
+        while(q.size() != 0 && !found) {
+            size = q.size();
+            for(int i = 0;i < size;i++) {
+                cur = q.poll();
+                HashSet<String> curSet = new HashSet<String>(cur);
+                last = cur.peekLast();
+                for(int j = 0;j < last.length();j++) {
+                    for(char k = 'a';k <= 'z';k++) {
+                        if(k != last.charAt(j)) {
+                            change = last.substring(0,j)+ k + last.substring(j+1,last.length());
+                            if(!change.equals(beginWord) && !curSet.contains(change) && set.contains(change)) {
+                                plus = (LinkedList)(cur.clone());
+                                plus.offerLast(change);
+                                q.offer(plus);
+                                if(change.equals(endWord)) found = true;
+                            }
+                        }
+                    }
+                }                                                                                               
+            }
+        }
+        for(LinkedList<String> list : q) {
+            if(list.peekLast().equals(endWord)) result.add(list);
+        }
+        return result;
+    }
 }
