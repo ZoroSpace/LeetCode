3ea2bd2137ced52005aa2ecaafba4dcc7d19ee1c
Zoro <ZoroSpace@users.noreply.github.com>
2018-10-16 19:25:39 +0800

    Update 206ReverseLinkedList.py

--- 206ReverseLinkedList.py
+++ 206ReverseLinkedList.py
@@ -19,3 +19,33 @@ class Solution:
             second.next=head
             head=first.next
         return second.next
+
+    
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def reverseList(self, head):
+        """
+        :type head: ListNode
+        :rtype: ListNode
+        """
+        first=ListNode(0)
+        first.next=head
+        second=ListNode(0)
+        
+        self.helper(first,second)
+        return second.next
+        
+        
+    def helper(self,first,second):
+        if first.next==None:
+            return
+        head=first.next
+        first.next=head.next
+        head.next=second.next
+        second.next=head
+        self.helper(first,second)
