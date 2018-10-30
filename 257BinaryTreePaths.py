3360889c69fe596f8554d5d0a81961b824b5e20b
Zoro <ZoroSpace@users.noreply.github.com>
2018-10-30 20:10:16 +0800

    Create 257BinaryTreePaths.py

---
+++ 257BinaryTreePaths.py
@@ -0,0 +1,47 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    ans = []
+    def binaryTreePaths(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[str]
+        """
+        if root == None:
+            return self.ans
+        path = []
+        path.append(root)
+        self.helper(root, path)
+        return self.ans
+    
+    
+    def helper(self,node,path):
+        if node.left == None and node.right == None:
+            s = "->"
+            vals = []
+            for i in path:
+                vals.append(str(i.val))
+            self.ans.append(s.join(vals))
+            return
+        if node.left != None:
+            path.append(node.left)
+            self.helper(node.left,path)
+            path.remove(node.left)
+        if node.right != None:
+            path.append(node.right)
+            self.helper(node.right,path)
+            path.remove(node.right)
+        return
+            
+        
+        
+            
+        
+        
+        
+        
