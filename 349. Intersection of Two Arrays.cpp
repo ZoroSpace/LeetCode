f7f105c2d42edde3e34e0467e09c9dc986b333e7
Zoro <ZoroSpace@users.noreply.github.com>
2019-03-16 15:59:36 +0800

    Update 349. Intersection of Two Arrays.cpp

--- 349. Intersection of Two Arrays.cpp
+++ 349. Intersection of Two Arrays.cpp
@@ -6,6 +6,30 @@ public:
         sort(nums2.begin(),nums2.end());
         int first = 0;
         int second = 0;
+        while((first < nums1.size()) && (second < nums2.size())) {
+            if(nums1[first] < nums2[second]) {
+                first++;
+            } else if(nums1[first] > nums2[second]) {
+                second++;
+            } else {
+                ans.push_back(nums1[first]);
+                second++;
+                while((second < nums2.size()) && (nums2[second] == nums2[second-1])) second++;
+            }
+        }
+        return ans;
+    }
+};
+
+
+class Solution {
+public:
+    vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
+        vector<int> ans;
+        sort(nums1.begin(),nums1.end());
+        sort(nums2.begin(),nums2.end());
+        int first = 0;
+        int second = 0;
         while((first < nums1.end()-nums1.begin()) && (second < nums2.end()-nums2.begin())) {
             if(nums1.at(first) < nums2.at(second)) {
                 first++;
