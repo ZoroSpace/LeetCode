825f641bae5ff20fe064745178123861e8d90ae3
Zoro <ZoroSpace@users.noreply.github.com>
2019-03-17 20:04:35 +0800

    Create 303. Range Sum Query - Immutable.cpp

---
+++ 303. Range Sum Query - Immutable.cpp
@@ -0,0 +1,39 @@
+class NumArray {
+public:
+    int *table;
+    NumArray(vector<int> nums) {
+        int n = nums.size();
+        table = new int[n+1]();
+        for(int i = 1;i <= n;i++) {
+            table[i] = table[i-1] + nums[i-1];
+        }
+    }
+    
+    int sumRange(int i, int j) {
+        return table[j+1] - table[i];
+    }
+};
+
+
+class NumArray {
+    
+    int[] table;
+
+    public NumArray(int[] nums) {
+        int n = nums.length;
+        table = new int[n+1];
+        for(int i = 1;i < n+1;i++) {
+            table[i] = table[i-1]+nums[i-1];
+        }
+    }
+    
+    public int sumRange(int i, int j) {
+        return table[j+1] - table[i];
+    }
+}
+
+/**
+ * Your NumArray object will be instantiated and called as such:
+ * NumArray obj = new NumArray(nums);
+ * int param_1 = obj.sumRange(i,j);
+ */
